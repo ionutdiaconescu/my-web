d775164d26a069374fb22a0efc2b02d069008fe3
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2026-02-08 13:30:00 +0200

    footer aded Text: “© 2026 Ionut Diaconescu. All rights reserved.”

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import SkillsSection from "./components/SkillsSection";
 import WorkSection from "./components/WorkSection";
 
 export default function Home() {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="min-h-screen w-full font-sans bg-(--background)">
       <main className="grid min-h-screen h-full w-full grid-cols-1 items-start justify-center gap-8 px-8 py-10 pt-24 min-[1400px]:grid-cols-[0.3fr_0.9fr]">
@@ -16,6 +17,9 @@ export default function Home() {
           <WorkSection />
         </section>
       </main>
+      <footer className="px-8 pb-8 text-center text-xs text-(--smoke)">
+        © {currentYear} Ionut Diaconescu. All rights reserved.
+      </footer>
     </div>
   );
 }
